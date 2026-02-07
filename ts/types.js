d45a7bb306ee4fe6d7b05c98c454bf4537db7aef
Puppy <pusanteemu@gmail.com>
2026-02-07 10:47:50 +0200

    remove js files from ts

--- ts/types.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.FlexDirection = void 0;
-var FlexDirection;
-(function (FlexDirection) {
-    FlexDirection["Row"] = "row";
-    FlexDirection["Column"] = "column";
-})(FlexDirection || (exports.FlexDirection = FlexDirection = {}));
